14584b9d4123c7efb36d624c72c4fce5b9f6d509
Abandoned Cart <twistedumbrella@gmail.com>
2021-10-15 08:13:35 -0400

    Update to StarKissed performance levels

--- ramdisk/overlay.d/sbin/init.starkissed.sh
+++ ramdisk/overlay.d/sbin/init.starkissed.sh
@@ -4,12 +4,13 @@ sleep 30;
 #
 ## Applying StarKissed Settings
 #
+
 ## Set TCP congestion algorithm
-#	echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control
-#
+# echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
+
 ## Setup Raccoon City Governor
-    echo "raccoon_city" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "raccoon_city" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-    echo "raccoon_city" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "raccoon_city" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 
-    echo "raccoon_city" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo "raccoon_city" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
